0cd451bfe3500fa00aaf2ba19112586dfeb5e77c
devcom33 <mouad.elb06@gmail.com>
2024-04-12 22:30:15 +0000

    add C character

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,8 +20,8 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
-   """ A function that return c """
-   return text.replace("_", " ")
+    """ A function that return c """
+    return "C" + " " + text.replace("_", " ")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
