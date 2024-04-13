c238ef78e66c55b1819a16e6f1bfdb6a57c4855c
devcom33 <mouad.elb06@gmail.com>
2024-04-13 00:33:56 +0000

    fix err

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,10 +36,11 @@ def is_num(n=0):
     """ A function that return numbers """
     return str(n) + " is a number"
 
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def temp_num(n):
     """ Return html """
-    render_template("5-number.html", n=n)
+    return render_template("5-number.html", n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
